de40cc4130f8a69c3569dcdc132aa69edc22a6ef
SwanyCastle <seunghwan712@gmail.com>
2024-03-07 16:14:58 +0900

    create books.py

---
+++ books.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, APIRouter
+
+
+BOOKS = [
+    {
+        "id": 1,
+        "title": "1984",
+        "author": "George Orwell",
+        "url": "https://example.com/1984",
+    },
+    {
+        "id": 2,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "url": "https://example.com/to-kill-a-mockingbird",
+    },
+    {
+        "id": 3,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "url": "https://example.com/the-great-gatsby",
+    },
+]
+
+app = FastAPI()
+router = APIRouter()
+
+@router.get('/', status_code=200)
+def main() -> dict:
+    return { "message": "Welcome to the game world!"}
+
+@router.get('/books', status_code=200)
+def fetch_books() -> list:
+    return BOOKS
+
+@router.get('/books/{book_id}', status_code=200)
+def fetch_book(book_id: int) -> dict:
+    for book in BOOKS:
+        if book['id'] == book_id:
+            return book
+    return {"error": "NotFound"}
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("books:app", reload=True, log_level='info')
--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = FastAPI()
 def index():
     return {'Hello World!'}
 
+# http://127.0.0.1:5000/items/3?query=asdasd - query 넣고싶은 경우
+@app.get('/items/{item_id}')
+def read_item(item_id: int, query: str=None) -> dict:
+    data = {'item_id': item_id, 'query': query}
+    return data
+
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", port=5000, log_level="debug", reload=True)
